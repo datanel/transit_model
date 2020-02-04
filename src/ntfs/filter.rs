3a7899b6f13e157fb4bbf80287f1361a7f8381c2
David Quintanel <david.quintanel@kisio.com>
2020-02-04 14:19:08 +0100

    add struct FilterProcessor

--- src/ntfs/filter.rs
+++ src/ntfs/filter.rs
@@ -63,6 +63,155 @@ impl Filter {
     }
 }
 
+struct FilterProcessor {
+    // model: Model,
+    calendars: CollectionWithId<Calendar>,
+    vjs: CollectionWithId<VehicleJourney>,
+    calendars_used: IdxSet<Calendar>,
+    vjs_used: IdxSet<VehicleJourney>,
+}
+
+impl FilterProcessor {
+    fn new(calendars: CollectionWithId<Calendar>, vjs: CollectionWithId<VehicleJourney>) -> Self {
+        Self {
+            calendars,
+            vjs,
+            calendars_used: BTreeSet::new(),
+            vjs_used: BTreeSet::new(),
+        }
+    }
+
+    fn apply(&mut self, model: &Model, filter: Filter) -> Result<Model> {
+        for (object_type, prop_values) in filter.filters {
+            match object_type {
+                ObjectType::Network => {
+                    let mut collection = model.networks.clone();
+
+                    let mut ids: HashSet<String> = HashSet::new();
+                    for (prop, values) in prop_values {
+                        ids = match prop.as_ref() {
+                            "network_id" => values
+                                .into_iter()
+                                .map(|id| match collection.get(&id) {
+                                    Some(_) => Ok(id.to_string()),
+                                    None => bail!("network {} not found.", id),
+                                })
+                                .collect::<Result<_>>()?,
+                            _ => bail!("property {} not found.", prop),
+                        };
+                    }
+
+                    self.union(model, &filter.action, &mut collection, ids);
+                }
+                ObjectType::Line => {
+                    let mut collection = model.lines.clone();
+
+                    let mut ids = HashSet::new();
+                    for (prop, values) in prop_values {
+                        ids = match prop.as_ref() {
+                            "line_code" => {
+                                let ids: HashSet<String> = collection
+                                    .values()
+                                    .filter(|l| {
+                                        let code = l.code.as_deref().unwrap_or("");
+                                        values.contains(code)
+                                    })
+                                    .map(|l| l.id.clone())
+                                    .collect();
+                                if ids.is_empty() {
+                                    bail!("no lines with property {} {:?} found.", prop, values);
+                                }
+
+                                ids
+                            }
+                            _ => bail!("property {} not found.", prop),
+                        };
+                    }
+                    self.union(model, &filter.action, &mut collection, ids);
+                }
+            };
+        }
+
+        Ok(self.finalize(model)?)
+    }
+
+    // find a better name
+    fn union<T>(
+        &mut self,
+        model: &Model,
+        action: &Action,
+        collection: &mut CollectionWithId<T>,
+        ids: HashSet<String>,
+    ) where
+        T: Id<T>,
+        IdxSet<T>: GetCorresponding<Calendar>,
+        IdxSet<T>: GetCorresponding<VehicleJourney>,
+    {
+        let id_to_old_idx = collection.get_id_to_idx().clone();
+        match action {
+            Action::Extract => collection.retain(|obj| ids.contains(obj.id())),
+            Action::Remove => collection.retain(|obj| !ids.contains(obj.id())),
+        }
+
+        let set_idx = collection
+            .values()
+            .map(|obj| id_to_old_idx[obj.id()])
+            .collect();
+        self.calendars_used = self
+            .calendars_used
+            .clone()
+            .union(&model.get_corresponding(&set_idx))
+            .cloned()
+            .collect();
+        self.vjs_used = self
+            .vjs_used
+            .clone()
+            .union(&model.get_corresponding(&set_idx))
+            .cloned()
+            .collect();
+    }
+
+    fn finalize(&self, model: &Model) -> Result<Model> {
+        let old_vj_idx_to_vj_id: HashMap<Idx<VehicleJourney>, String> = self
+            .vjs
+            .get_id_to_idx()
+            .iter()
+            .map(|(id, &idx)| (idx, id.clone()))
+            .collect();
+        let mut collections = model.into_collections();
+
+        collections.calendars.retain(|c| {
+            self.calendars_used
+                .contains(&self.calendars.get_idx(&c.id).unwrap())
+        });
+
+        collections
+            .vehicle_journeys
+            .retain(|c| self.vjs_used.contains(&self.vjs.get_idx(&c.id).unwrap()));
+        collections.stop_time_ids = updated_stop_time_attributes(
+            &collections.vehicle_journeys,
+            &collections.stop_time_ids,
+            &old_vj_idx_to_vj_id,
+        );
+        collections.stop_time_headsigns = updated_stop_time_attributes(
+            &collections.vehicle_journeys,
+            &collections.stop_time_headsigns,
+            &old_vj_idx_to_vj_id,
+        );
+        collections.stop_time_comments = updated_stop_time_attributes(
+            &collections.vehicle_journeys,
+            &collections.stop_time_comments,
+            &old_vj_idx_to_vj_id,
+        );
+
+        if collections.calendars.is_empty() {
+            bail!("the data does not contain services anymore.")
+        }
+
+        Ok(Model::new(collections)?)
+    }
+}
+
 fn updated_stop_time_attributes<T>(
     vehicle_journeys: &CollectionWithId<VehicleJourney>,
     attributes_map: &HashMap<(Idx<VehicleJourney>, u32), T>,
@@ -83,146 +232,10 @@ where
     updated_attributes_map
 }
 
-fn i_do_not_know_how_to_name_it<T: Id<T>>(
-    action: &Action,
-    model: &Model,
-    collection: &mut CollectionWithId<T>,
-    ids: HashSet<String>,
-    calendars_used: &mut IdxSet<Calendar>,
-    vjs_used: &mut IdxSet<VehicleJourney>,
-) where
-    IdxSet<T>: GetCorresponding<Calendar>,
-    IdxSet<T>: GetCorresponding<VehicleJourney>,
-{
-    let id_to_old_idx = collection.get_id_to_idx().clone();
-    match action {
-        Action::Extract => collection.retain(|obj| ids.contains(obj.id())),
-        Action::Remove => collection.retain(|obj| !ids.contains(obj.id())),
-    }
-
-    let set_idx = collection
-        .values()
-        .map(|obj| id_to_old_idx[obj.id()])
-        .collect();
-    *calendars_used = calendars_used
-        .clone()
-        .union(&model.get_corresponding(&set_idx))
-        .cloned()
-        .collect();
-    *vjs_used = vjs_used
-        .clone()
-        .union(&model.get_corresponding(&set_idx))
-        .cloned()
-        .collect();
-}
-
 /// Extract or remove or networks/lines
 pub fn filter(model: Model, filter: Filter) -> Result<Model> {
     let calendars = model.calendars.clone();
     let vjs = model.vehicle_journeys.clone();
-    let old_vj_idx_to_vj_id: HashMap<Idx<VehicleJourney>, String> = model
-        .vehicle_journeys
-        .get_id_to_idx()
-        .iter()
-        .map(|(id, &idx)| (idx, id.clone()))
-        .collect();
-
-    let mut calendars_used: IdxSet<Calendar> = BTreeSet::new();
-    let mut vjs_used: IdxSet<VehicleJourney> = BTreeSet::new();
-
-    for (object_type, prop_values) in filter.filters {
-        match object_type {
-            ObjectType::Network => {
-                let mut collection = model.networks.clone();
-
-                let mut ids: HashSet<String> = HashSet::new();
-                for (prop, values) in prop_values {
-                    ids = match prop.as_ref() {
-                        "network_id" => values
-                            .into_iter()
-                            .map(|id| match collection.get(&id) {
-                                Some(_) => Ok(id.to_string()),
-                                None => bail!("network {} not found.", id),
-                            })
-                            .collect::<Result<_>>()?,
-                        _ => bail!("property {} not found.", prop),
-                    };
-                }
-
-                i_do_not_know_how_to_name_it(
-                    &filter.action,
-                    &model,
-                    &mut collection,
-                    ids,
-                    &mut calendars_used,
-                    &mut vjs_used,
-                );
-            }
-            ObjectType::Line => {
-                let mut collection = model.lines.clone();
-
-                let mut ids = HashSet::new();
-                for (prop, values) in prop_values {
-                    ids = match prop.as_ref() {
-                        "line_code" => {
-                            let ids: HashSet<String> = collection
-                                .values()
-                                .filter(|l| {
-                                    let code = l.code.as_deref().unwrap_or("");
-                                    values.contains(code)
-                                })
-                                .map(|l| l.id.clone())
-                                .collect();
-                            if ids.is_empty() {
-                                bail!("no lines with property {} {:?} found.", prop, values);
-                            }
-
-                            ids
-                        }
-                        _ => bail!("property {} not found.", prop),
-                    };
-                }
-                i_do_not_know_how_to_name_it(
-                    &filter.action,
-                    &model,
-                    &mut collection,
-                    ids,
-                    &mut calendars_used,
-                    &mut vjs_used,
-                );
-            }
-        };
-    }
-
-    let mut collections = model.into_collections();
-
-    collections
-        .calendars
-        .retain(|c| calendars_used.contains(&calendars.get_idx(&c.id).unwrap()));
-
-    collections
-        .vehicle_journeys
-        .retain(|c| vjs_used.contains(&vjs.get_idx(&c.id).unwrap()));
-
-    collections.stop_time_ids = updated_stop_time_attributes(
-        &collections.vehicle_journeys,
-        &collections.stop_time_ids,
-        &old_vj_idx_to_vj_id,
-    );
-    collections.stop_time_headsigns = updated_stop_time_attributes(
-        &collections.vehicle_journeys,
-        &collections.stop_time_headsigns,
-        &old_vj_idx_to_vj_id,
-    );
-    collections.stop_time_comments = updated_stop_time_attributes(
-        &collections.vehicle_journeys,
-        &collections.stop_time_comments,
-        &old_vj_idx_to_vj_id,
-    );
-
-    if collections.calendars.is_empty() {
-        bail!("the data does not contain services anymore.")
-    }
-
-    Model::new(collections)
+    let mut processor = FilterProcessor::new(calendars, vjs);
+    Ok(processor.apply(&model, filter)?)
 }
